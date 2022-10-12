78352acd21a8af59d5f3067b20058d4cb9ce85a4
Thomas Mertens <thomas.mertens7@outlook.com>
2022-10-12 12:21:09 +0800

    init

---
+++ MESI.py
@@ -0,0 +1,48 @@
+from multiprocessing import Process
+import sys
+
+
+def MESI(n, input_file, cache_size, associativity, block_size):
+    print("MESI: " + n)
+    with open('blackscholes_0.data', 'r') as f:
+        f_contents = f.read()
+        print(f_contents)
+
+    print(f.name)
+
+
+
+def Dragon(n, input_file, cache_size, associativity, block_size):
+    print("Dragon: " + n)
+    
+
+
+
+
+
+
+if len(sys.argv != 5):
+    raise Exception("Invalid number of arguments")
+
+protocol = sys.argv[1]
+input_file = sys.argv[2]
+cache_size = sys.argv[3]
+associativity = sys.argv[4]
+block_size = sys.argv[5]
+
+if protocol == "MESI":
+    p = [None,None,None,None]
+    for n in range(4):
+        p[n] = Process(target=MESI, args=(n, input_file, cache_size, associativity, block_size))
+        p[n].start()
+        p[n].join()
+
+elif protocol == "Dragon":
+    p = [None,None,None,None]
+    for n in range(4):
+        p[n] = Process(target=Dragon, args=(n, input_file, cache_size, associativity, block_size))
+        p[n].start()
+        p[n].join()
+
+else:
+    raise Exception("Invalid protocol")
